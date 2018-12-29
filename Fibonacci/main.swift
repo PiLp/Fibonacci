5e3dfd746342354ee2444a26ec1ca04d7cdb99fe
Philip Håkans <philip.hakans@gmail.com>
2018-12-29 18:10:35 +0100

    Initial Commit

---
+++ Fibonacci/main.swift
@@ -0,0 +1,24 @@
+//
+//  main.swift
+//  Fibonacci
+//
+//  Created by Philip Håkans on 2018-12-29.
+//  Copyright © 2018 Philip Håkans. All rights reserved.
+//
+
+import Foundation
+
+func Fibonacci(_ n: Int) -> Int {
+
+	if n <= 2 {
+		return 1
+	}
+	else { 
+		return Fibonacci(n - 1) + Fibonacci(n - 2)
+	}
+}
+
+for i in 1...20 {
+	print(Fibonacci(i))
+}
+
